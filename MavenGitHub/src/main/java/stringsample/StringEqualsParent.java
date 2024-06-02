c0c59d91065b31a51c46c3b2708b258b6fcbfc9d
1233 <1233@DESKTOP-PMNJABA>
2024-06-02 20:36:16 +0400

    Added new variables a,b and a string method valueof.

--- MavenGitHub/src/main/java/stringsample/StringEqualsParent.java
+++ MavenGitHub/src/main/java/stringsample/StringEqualsParent.java
@@ -4,6 +4,8 @@ public class StringEqualsParent {
 	String s1="hello";
 	String s2=new String("hello");
 	String s3="hello";
+	float a=80.08f;
+	int b=60;
 	public void stringEquals()
 	{
 		System.out.println("==used to compare the referance of two objeact in java");
@@ -12,6 +14,7 @@ public class StringEqualsParent {
 		
 		System.out.println("eqauals method compares two strings & returns true if both are equal");
 		System.out.println(s1.equals(s3));
+		System.out.println(s1+String.valueOf(a)+b);
 	}
 
 }
